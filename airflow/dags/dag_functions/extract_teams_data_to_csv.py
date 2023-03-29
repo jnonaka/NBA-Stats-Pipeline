c1db0464a8bad305727832143e5ac20671376131
jnonaka <nonakajay@hotmail.com>
2023-03-28 18:09:18 -0600

    Remove exec_date arg

--- airflow/dags/dag_functions/extract_teams_data_to_csv.py
+++ airflow/dags/dag_functions/extract_teams_data_to_csv.py
@@ -16,23 +16,22 @@ def extract_to_df(api_response):
     out = pd.DataFrame(api_response)
     return out[(out['nbaFranchise'] == True) & (out['allStar'] == False)]
 
-def format_csv(df, date):
+def format_csv(df):
     df.to_csv(
-        f"{AIRFLOW_HOME}/teams_{date}.csv",
+        f"{AIRFLOW_HOME}/teams.csv",
         index=False
     )
 
-def main(xcom_json, date):
+def main(xcom_json):
     teams_df = extract_to_df(json.loads(xcom_json))
-    format_csv(teams_df, date)
+    format_csv(teams_df)
 
 if __name__ == '__main__':
     
     try:
         xcom_json = json.loads(sys.argv[1])
-        exec_date = str(sys.argv[2])
     except Exception as e:
         print(f"Error detected with input. Error: {e}")
         sys.exit(1)
     
-    main(xcom_json, exec_date)
+    main(xcom_json)
